cfbfc4f4b1bdce28119d91280274ada5681d44c0
Mateo Vrtunski <mateo.froxi@gmail.com>
2025-04-07 12:05:55 +0200

    ajmoooooooooo

--- Python/dostop.py
+++ Python/dostop.py
@@ -1,16 +1,20 @@
-
+import os 
 db = 'sem2024_mateov'
 host = 'baza.fmf.uni-lj.si'
 user = 'javnost'
 password = 'javnogeslo'
 
+
+
+DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 def ustvari_povezavo():
     import psycopg2
     conn = psycopg2.connect(
         host=host,
         database=db,  # zamenjaj s pravim imenom baze
         user=user,  # zamenjaj s pravim uporabnikom
-        password=password  # zamenjaj s pravim geslom
+        password=password,
+        port = DB_PORT  # zamenjaj s pravim geslom
     )
     cur = conn.cursor()  # Ustvari kurzor za izvajanje poizvedb
     return conn, cur
